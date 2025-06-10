a4787133ae683c6ac7fb4720378dc3d106575664
Sajid-developer <msasajid01.pro@gmail.com>
2025-06-10 19:02:36 +0530

    Added code to enable speech recognition feature.

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("App running successfully...");
+
+let msg=document.querySelector(".message");
+let scroll=msg.scrollHeight;
+
+// code to enable speech recognition feature ✅
+    function getVoice(){
+        let voice=new webkitSpeechRecognition();
+        voice.lang="en-GB";
+        voice.onresult=(e) =>{
+        msg.value+=e.results[0][0].transcript + "." + " ";
+        msg.scrollTo(0,scroll);
+      }
+        voice.start();
+    }
